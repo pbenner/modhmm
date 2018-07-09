66376508871a0545cf8e67ad0b4600f9a7e9a7de
Philipp Benner <benner@molgen.mpg.de>
2018-07-09 10:38:53 +0200

    2018/07/09-10:38:53 (Linux zoophobie.molgen.mpg.de unknown)

--- modhmm_multi_feature_classify.go
+++ modhmm_multi_feature_classify.go
@@ -34,8 +34,18 @@ import   "github.com/pborman/getopt"
 
 /* -------------------------------------------------------------------------- */
 
-func multi_feature_classify(config ConfigModHmm, classifier MatrixBatchClassifier, trackFiles []string, result1, result2 string) {
-  result, err := ImportAndBatchClassifyMultiTrack(config.SessionConfig, classifier, trackFiles, false); if err != nil {
+func multi_feature_classify(config ConfigModHmm, classifier MatrixBatchClassifier, trackFiles []string, tracks []Track, result1, result2 string) []Track {
+  if len(tracks) != len(trackFiles) {
+    tracks := make([]Track, len(trackFiles))
+    for i, filename := range trackFiles {
+      if t, err := ImportTrack(config.SessionConfig, filename); err != nil {
+        log.Fatal(err)
+      } else {
+        tracks[i] = t
+      }
+    }
+  }
+  result, err := BatchClassifyMultiTrack(config.SessionConfig, classifier, tracks, false); if err != nil {
     log.Fatal(err)
   }
   if err := ExportTrack(config.SessionConfig, result, result1); err != nil {
@@ -47,11 +57,12 @@ func multi_feature_classify(config ConfigModHmm, classifier MatrixBatchClassifie
   if err := ExportTrack(config.SessionConfig, result, result2); err != nil {
     log.Fatal(err)
   }
+  return tracks
 }
 
 /* -------------------------------------------------------------------------- */
 
-func modhmm_multi_feature_classify(config ConfigModHmm, state string) {
+func modhmm_multi_feature_classify(config ConfigModHmm, state string, tracks []Track) []Track {
 
   localConfig := config
   localConfig.BinSummaryStatistics = "mean"
@@ -86,49 +97,60 @@ func modhmm_multi_feature_classify(config ConfigModHmm, state string) {
   switch strings.ToLower(state) {
   case "pa":
     filenameResult1 = config.MultiFeatureClass.PA
-    filenameResult1 = config.MultiFeatureClassExp.PA
+    filenameResult2 = config.MultiFeatureClassExp.PA
     classifier = ClassifierPA{}
   case "pb":
     filenameResult1 = config.MultiFeatureClass.PB
-    filenameResult1 = config.MultiFeatureClassExp.PB
+    filenameResult2 = config.MultiFeatureClassExp.PB
     classifier = ClassifierPB{}
   case "ea":
     filenameResult1 = config.MultiFeatureClass.EA
-    filenameResult1 = config.MultiFeatureClassExp.EA
+    filenameResult2 = config.MultiFeatureClassExp.EA
     classifier = ClassifierEA{}
   case "ep":
     filenameResult1 = config.MultiFeatureClass.EP
-    filenameResult1 = config.MultiFeatureClassExp.EP
+    filenameResult2 = config.MultiFeatureClassExp.EP
     classifier = ClassifierEP{}
   case "tr":
     filenameResult1 = config.MultiFeatureClass.TR
-    filenameResult1 = config.MultiFeatureClassExp.TR
+    filenameResult2 = config.MultiFeatureClassExp.TR
     classifier = ClassifierTR{}
   case "tl":
     filenameResult1 = config.MultiFeatureClass.TL
-    filenameResult1 = config.MultiFeatureClassExp.TL
+    filenameResult2 = config.MultiFeatureClassExp.TL
     classifier = ClassifierTL{}
   case "r1":
     filenameResult1 = config.MultiFeatureClass.R1
-    filenameResult1 = config.MultiFeatureClassExp.R1
+    filenameResult2 = config.MultiFeatureClassExp.R1
     classifier = ClassifierR1{}
   case "r2":
     filenameResult1 = config.MultiFeatureClass.R2
-    filenameResult1 = config.MultiFeatureClassExp.R2
+    filenameResult2 = config.MultiFeatureClassExp.R2
     classifier = ClassifierR2{}
   case "ns":
     filenameResult1 = config.MultiFeatureClass.NS
-    filenameResult1 = config.MultiFeatureClassExp.NS
+    filenameResult2 = config.MultiFeatureClassExp.NS
     classifier = ClassifierNS{}
   case "cl":
     filenameResult1 = config.MultiFeatureClass.CL
-    filenameResult1 = config.MultiFeatureClassExp.CL
+    filenameResult2 = config.MultiFeatureClassExp.CL
     classifier = ClassifierCL{}
   default:
     log.Fatalf("unknown state: %s", state)
   }
+  if updateRequired(config, filenameResult1, trackFiles...) ||
+    (updateRequired(config, filenameResult2, trackFiles...)) {
+    modhmm_single_feature_classify_all(config)
+    tracks = multi_feature_classify(localConfig, classifier, trackFiles, tracks, filenameResult1, filenameResult2)
+  }
+  return tracks
+}
 
-  multi_feature_classify(localConfig, classifier, trackFiles, filenameResult1, filenameResult2)
+func modhmm_multi_feature_classify_all(config ConfigModHmm) {
+  var tracks []Track
+  for _, feature := range []string{"pa", "pb", "ea", "ep", "tr", "tl", "r1", "r2", "ns", "cl"} {
+    tracks = modhmm_multi_feature_classify(config, feature, tracks)
+  }
 }
 
 /* -------------------------------------------------------------------------- */
@@ -149,10 +171,13 @@ func modhmm_multi_feature_classify_main(config ConfigModHmm, args []string) {
     os.Exit(0)
   }
   // command arguments
-  if len(options.Args()) != 1 {
+  if len(options.Args()) > 1 {
     options.PrintUsage(os.Stderr)
     os.Exit(1)
   }
-
-  modhmm_multi_feature_classify(config, options.Args()[0])
+  if len(options.Args()) == 0 {
+    modhmm_multi_feature_classify_all(config)
+  } else {
+    modhmm_multi_feature_classify(config, options.Args()[0], nil)
+  }
 }
